a8e441bc45d37cfdf419c14930ff0df6089d8268
jonghoon lee <jonghoon7431@gmail.com>
2024-05-23 10:51:53 +0900

    feat : ex1 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to="details">detail로 이동하기</Link>
+    </div>
+  );
 };
 
 export default Home;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -8,7 +8,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="detail" element={<Detail />} />
+        <Route path="details/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
